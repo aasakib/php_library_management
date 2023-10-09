2dcbe8253ed0cbaf88ccffd9305e1243fe99b823
aasakib <johnsakibcharls@gmail.com>
2023-10-09 18:25:37 +0600

    first commit

---
+++ includes/header.php
@@ -0,0 +1,95 @@
+<div class="navbar navbar-inverse set-radius-zero">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <div class="navbar-brand">
+                <div style="display: flex; align-items: center;">
+                    <img src="assets/img/images.jpg" height="125" width="150" style="margin-right: 30px;" />
+                    <div>
+                        <h1 style="margin: 0; padding: 0; margin-top: -25px;">
+                            <span id="brand-text">BAUST Online Library Management System</span>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+            
+<?php if($_SESSION['login'])
+{
+?> 
+            <div class="right-div">
+                <a href="logout.php" class="btn btn-danger pull-right">LOG ME OUT</a>
+            </div>
+            <?php }?>
+        </div>
+    </div>
+    <!-- LOGO HEADER END-->
+<?php if($_SESSION['login'])
+{
+?>  
+
+<section class="menu-section">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                            <li><a href="dashboard.php" class="menu-top-active">DASHBOARD</a></li>
+                            <li><a href="issued-books.php">Issued Books</a></li>
+                            <section>
+                
+                             <li>
+                                <a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Account <i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="my-profile.php">My Profile</a></li>
+                                     <li role="presentation"><a role="menuitem" tabindex="-1" href="change-password.php">Change Password</a></li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+   
+    <?php } else { ?>
+        <section class="menu-section">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">                        
+                          
+      <li><a href="index.php">Home</a></li>
+      <li><a href="index.php#ulogin">User Login</a></li>
+                            <li><a href="signup.php">User Signup</a></li>
+                         
+                            <li><a href="adminlogin.php">Admin Login</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <section style="margin-left: 100px;">
+    <div class="box">
+        <h4>Open at: 08:00 am</h4>
+        <h4>Close at: 05:00 pm</h4><br>
+    </div>
+</section>
+
+
+    <?php } ?>
